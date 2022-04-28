fd2b7bfafa54944b4df2b96e0e209c669f2c1716
ahmed4403 <4403@holbertonschool.com>
2022-04-28 10:59:18 +0000

    add

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from add_0 import add
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    one = 1
+    two = 2
+    print(f"{one} + {two} = {add(one, two)}")
+    main()
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    num1 = 10
+    num2 = 5
+    print(f"{num1} + {num2} = {add(num1, num2)}")
+    print(f"{num1} - {num2} = {sub(num1, num2)}")
+    print(f"{num1} * {num2} = {mul(num1, num2)}")
+    print(f"{num1} / {num2} = {div(num1, num2)}")
+    main()
