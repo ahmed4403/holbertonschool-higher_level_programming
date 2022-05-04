b89f190be5b3ce21e4a1d72e50e37c40eb4bcbd5
ahmed4403 <4403@holbertonschool.com>
2022-05-04 13:44:52 +0000

    test

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -21,15 +21,15 @@ def roman_to_int(roman_string):
         for j in roman_string:
             if j == "I" and roman_string[index + 1:2] == "V":
                 result -= 2
-            if j == "I" and roman_string[index + 1:2] == "X":
+            elif j == "I" and roman_string[index + 1:2] == "X":
                 result -= 2
-            if j == "X" and roman_string[index + 1:2] == "L":
+            elif j == "X" and roman_string[index + 1:2] == "L":
                 result -= 20
-            if j == "X" and roman_string[index + 1:2] == "C":
+            elif j == "X" and roman_string[index + 1:2] == "C":
                 result -= 20
-            if j == "C" and roman_string[index + 1:2] == "D":
+            elif j == "C" and roman_string[index + 1:2] == "D":
                 result -= 200
-            if j == "C" and roman_string[index + 1:2] == "M":
+            elif j == "C" and roman_string[index + 1:2] == "M":
                 result -= 200
             index += 1
         return result
