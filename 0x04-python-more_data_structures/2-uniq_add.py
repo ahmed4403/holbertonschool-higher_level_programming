12c55d1e3f7c16c6dcab0fec58cb3f06d36b613e
ahmed4403 <4403@holbertonschool.com>
2022-05-04 11:31:46 +0000

    test

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-        newSet = set(my_list)
-        tmp = 0
-        for i in newSet:
-            i = i + tmp
-            tmp = i
-        return i
+    newSet = set(my_list)
+    tmp = 0
+    for i in newSet:
+        tmp = tmp + i
+    return tmp
