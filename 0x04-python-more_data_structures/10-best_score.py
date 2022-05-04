0927a7548399249f00f664846acf276a81e5ad96
ahmed4403 <4403@holbertonschool.com>
2022-05-04 11:36:44 +0000

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,11 +2,4 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    name = next(iter(a_dictionary))
-    biggest = a_dictionary.get(name)
-    for key, value in a_dictionary.items():
-        if value > biggest:
-            biggest = value
-            name = key
-    return name
-
+    return max(a_dictionary)
