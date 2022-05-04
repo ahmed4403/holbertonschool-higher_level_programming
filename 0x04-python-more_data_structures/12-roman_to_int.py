aff69be1ef705e7455932c2f551eb2bd2244f18d
ahmed4403 <4403@holbertonschool.com>
2022-05-04 13:16:11 +0000

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) == str and roman_string is not None:
+        result = 0
+        index = 0
+        for i in roman_string:
+            if i == "I":
+                result += 1
+            elif i == "V":
+                result += 5
+            elif i == "X":
+                result += 10
+            elif i == "L":
+                result += 50
+            elif i == "C":
+                result += 100
+            elif i == "D":
+                result += 500
+            elif i == "M":
+                result += 1000
+        for j in roman_string:
+            if j == "I" and roman_string[index + 1:2] == "V":
+                result -= 2
+            if j == "I" and roman_string[index + 1:2] == "X":
+                result -= 2
+        return result
+    return 0
