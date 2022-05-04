260b5ed5cce6f1d9c7068ab26a1d7fa5d4a3644e
ahmed4403 <4403@holbertonschool.com>
2022-05-04 11:44:37 +0000

    test

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return max(a_dictionary) if a_dictionary else None
+    def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    name = next(iter(a_dictionary))
+    biggest = a_dictionary.get(name)
+    for key, value in a_dictionary.items():
+        if value > biggest:
+            biggest = value
+            name = key
+    return name
