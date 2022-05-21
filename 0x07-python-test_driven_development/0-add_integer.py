52e00841e68a35142be58c50f484072590697c39
ahmed4403 <4403@holbertonschool.com>
2022-05-21 21:50:34 +0000

    add

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
-    if type(a) is not int and type(a) is not float:
+    if type(a) is not int and type(a) is not float or a != a or \
+       a == float("inf") or a == float("-inf"):
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if type(b) is not int and type(b) is not float or b != b or \
+       b == float("inf") or b == float("-inf"):
         raise TypeError("b must be an integer")
-    return int(a) + int(b) 
+    return int(a) + int(b)
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,11 +15,11 @@ integers/floats")
     for j in matrix:
         for k in j:
             if type(k) is not int and type(k) is not float or k != k or \
-               k == float("inf"):
+               k == float("inf") or k == float("-inf"):
                 raise TypeError("matrix must be a matrix (list of lists) of \
 integers/floats")
     if type(div) is not int and type(div) is not float or div != div or \
-       div === float("inf"):
+       div == float("inf") or div == float("-inf"):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
