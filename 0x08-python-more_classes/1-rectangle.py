6a11ef104dc0e4ff7ff885e75d87670da2dc19cc
ahmed4403 <4403@holbertonschool.com>
2022-05-23 19:00:02 +0000

    test

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     '''This is a class'''
     def __init__(self, width=0, height=0):
         '''This is a method'''
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
